16bcba84465f4292373bf13777a50690b3d9077d
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-30 18:43:18 -0300

    chore: change App component and its tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { App } from './App';
+
+const fetchMock = vitest.fn(() => Promise.resolve({
+	...global.fetch.prototype,
+	json: () => Promise.resolve({ cep: '00000-000', uf: 'SP' })
+}));
+
+global.fetch = fetchMock;
+
+beforeEach(() => {
+	fetchMock.mockClear();
+});
 
 describe('<App />', () => {
 	it('Should render it', () => {
@@ -40,5 +51,60 @@ describe('<App />', () => {
 			name: 'example2',
 			email: 'email@email.com'
 		});
+	});
+
+	it("Should call console.log function passing It's São Paulo string", async () => {
+		const spy = vitest.spyOn(console, 'log').mockImplementation(() => {}); // eslint-disable-line
+		const { container } = render(<App />);
+		const form = container.querySelector('form') as HTMLFormElement;
+		const nameInput = form.querySelector('input[name=name]') as HTMLInputElement;
+		const emailInput = form.querySelector('input[name=email]') as HTMLInputElement;
+		const submitButton = form.querySelector('button') as HTMLButtonElement;
+
+		await userEvent.type(nameInput, 'example1');
+		await userEvent.type(emailInput, 'email@email.com');
+		await userEvent.click(submitButton);
+
+		expect(fetch).toHaveBeenCalledTimes(1);
+		expect(fetch).toHaveBeenCalledWith('https://viacep.com.br/ws/01001000/json/');
+		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy).toHaveBeenCalledWith("It's São Paulo");
+	});
+
+	it("Should call console.log function passing It's not São Paulo string", async () => {
+		fetchMock.mockImplementation(() => Promise.resolve({
+			...global.fetch.prototype,
+			json: () => Promise.resolve({ cep: '11111-111', uf: 'MG' })
+		}));
+
+		const spy = vitest.spyOn(console, 'log').mockImplementation(() => {}); // eslint-disable-line
+		const { container } = render(<App />);
+		const form = container.querySelector('form') as HTMLFormElement;
+		const nameInput = form.querySelector('input[name=name]') as HTMLInputElement;
+		const emailInput = form.querySelector('input[name=email]') as HTMLInputElement;
+		const submitButton = form.querySelector('button') as HTMLButtonElement;
+
+		await userEvent.type(nameInput, 'example1');
+		await userEvent.type(emailInput, 'email@email.com');
+		await userEvent.click(submitButton);
+
+		expect(fetch).toHaveBeenCalledTimes(1);
+		expect(fetch).toHaveBeenCalledWith('https://viacep.com.br/ws/01001000/json/');
+		expect(spy).toHaveBeenCalledTimes(2);
+		expect(spy).toHaveBeenCalledWith("It's not São Paulo");
+	});
+
+	it('Should render input errors after trying to submit the form without any content', async () => {
+		const { container } = render(<App />);
+		const form = container.querySelector('form') as HTMLFormElement;
+		const submitButton = form.querySelector('button') as HTMLButtonElement;
+
+		await userEvent.click(submitButton);
+
+		const nameInputErrorElement = await screen.findByText('name is a required field');
+		const emailInputErrorElement = await screen.findByText('email is a required field');
+
+		expect(nameInputErrorElement).toBeInTheDocument();
+		expect(emailInputErrorElement).toBeInTheDocument();
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,58 @@
-import { FormEvent, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export function App() {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			email: ''
+		},
+		validationSchema: Yup.object({
+			name: Yup.string().required(),
+			email: Yup.string().email().required()
+		}),
+		async onSubmit(values) {
+			if(values.name === 'example1')
+				console.log('this is the first example');
+			else
+				console.log(values);
 
-	function handleSubmit(event: FormEvent) {
-		event.preventDefault();
+			const result = await fetch('https://viacep.com.br/ws/01001000/json/');
+			const data = await result.json();
 
-		if(name === 'example1')
-			console.log('this is the first example');
-		else
-			console.log({ name, email });
-	}
+			if(data.uf === 'SP')
+				console.log("It's São Paulo");
+			else
+				console.log("It's not São Paulo");
+		}
+	});
 
 	return (
 		<>
 			<h1>Vitest Example</h1>
 
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					name="name"
-					placeholder="Name"
-					onChange={event => setName(event.target.value)}
-					value={name}
-				/>
+			<form onSubmit={formik.handleSubmit} noValidate>
+				<div>
+					<input
+						type="text"
+						name="name"
+						placeholder="Name"
+						onChange={formik.handleChange}
+						value={formik.values.name}
+					/>
+					{formik.touched.name && formik.errors.name && <span style={{ color: 'red' }}>{formik.errors.name}</span>}
+				</div>
 
-				<input
-					type="email"
-					name="email"
-					placeholder="Email"
-					onChange={event => setEmail(event.target.value)}
-					value={email}
-				/>
+				<div>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						onChange={formik.handleChange}
+						value={formik.values.email}
+					/>
+					{formik.touched.email && formik.errors.email && <span style={{ color: 'red' }}>{formik.errors.email}</span>}
+				</div>
 
 				<button type="submit">Save</button>
 			</form>
