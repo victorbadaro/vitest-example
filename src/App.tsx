ea425fec0f17d98f5f4d70ecd07d6e2f873440f8
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2022-10-30 15:23:01 -0300

    feat: add a form and its submit function in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,38 @@
+import { FormEvent, useState } from 'react';
+
 export function App() {
-	return <h1>Vitest Example</h1>;
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+
+	function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+
+		console.log({ name, email });
+	}
+
+	return (
+		<>
+			<h1>Vitest Example</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="name"
+					placeholder="Name"
+					onChange={event => setName(event.target.value)}
+					value={name}
+				/>
+
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+					onChange={event => setEmail(event.target.value)}
+					value={email}
+				/>
+
+				<button type="submit">Save</button>
+			</form>
+		</>
+	);
 }
